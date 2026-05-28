50eab5d7ef48cfd22002a6c51b76625e3d9d5c5f
Vigna Rubash Ram <rubash304@gmail.com>
2026-05-28 07:58:01 +0000

    add source yaml files

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,22 @@
+with source as (
+
+    select * from dbt-analytics-495608.stripe.payments
+),
+
+renamed as (
+
+    select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount as payment_amount,
+    created as payment_created
+
+    from source
+)
+
+select * from renamed
+
+
+
